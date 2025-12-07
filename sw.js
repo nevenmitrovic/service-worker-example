f7e1a2f31f3f4eaaa22d3e2aee8ba69c3d544667
neven <nevenmitrovic4@gmail.com>
2025-12-07 17:11:22 +0100

    Add assets to cache

---
+++ sw.js
@@ -0,0 +1,20 @@
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/style.css",
+      "/app.js",
+      "/image-list.js",
+      "/star-wars-logo.jpg",
+      "/gallery/bountyHunters.jpg",
+      "/gallery/myLittleVader.jpg",
+      "/gallery/snowTroopers.jpg",
+    ]),
+  );
+});
