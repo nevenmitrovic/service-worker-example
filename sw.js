7c9e98ee3bbac3ef607270afae03a6c12cdffcaa
neven <nevenmitrovic4@gmail.com>
2025-12-07 17:32:58 +0100

    Add cacheFirst & putInCache

--- sw.js
+++ sw.js
@@ -3,6 +3,41 @@ const addResourcesToCache = async (resources) => {
   await cache.addAll(resources);
 };
 
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, fallbackUrl, event }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    event.waitUntil(putInCache(request, responseFromNetwork.clone()));
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     addResourcesToCache([
@@ -17,4 +52,8 @@ self.addEventListener("install", (event) => {
       "/gallery/snowTroopers.jpg",
     ]),
   );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst(event.request, event));
 });
